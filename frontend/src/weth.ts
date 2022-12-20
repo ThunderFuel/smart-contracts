f11fc00c75cfd64eee71ec90e9e5222966a6c1a5
Meliksah Gurtemel <meliksahgurtemel01@gmail.com>
2022-12-20 13:49:27 +0300

    update: completed weth contract interaction

--- frontend/src/weth.ts
+++ frontend/src/weth.ts
@@ -94,12 +94,12 @@ export async function deposit(
         const wallet = new BaseWalletUnlocked(walletPublicKey);
         const contract = WethAbi__factory.connect(contractId, wallet);
         const coin: CoinQuantityLike = { amount: depositAmount, assetId: NativeAssetId}
-        const { value } = await contract.functions
+        const { logs } = await contract.functions
             .deposit()
             .txParams({gasPrice: 1})
             .callParams({forward: coin})
             .call();
-        return value;
+        return logs;
     } catch(err: any) {
         console.error("WETH TS SDK: " + err);
         return err;
@@ -114,11 +114,118 @@ export async function withdraw(
     try {
         const wallet = new BaseWalletUnlocked(walletPublicKey);
         const contract = WethAbi__factory.connect(contractId, wallet);
-        const { value } = await contract.functions
+        const { logs } = await contract.functions
             .withdraw(withdrawAmount)
             .txParams({gasPrice: 1, variableOutputs: 1})
             .call();
-        return value;
+        return logs;
+    } catch(err: any) {
+        console.error("WETH TS SDK: " + err);
+        return err;
+    }
+}
+
+export async function approve(
+    contractId: string,
+    walletPublicKey: string,
+    spender: string,
+    amount: number,
+) {
+    try {
+        const wallet = new BaseWalletUnlocked(walletPublicKey);
+        const contract = WethAbi__factory.connect(contractId, wallet);
+        const _spender: IdentityInput = { ContractId: { value: spender } };
+        const { value, logs } = await contract.functions
+            .approve(_spender, amount)
+            .txParams({gasPrice: 1, variableOutputs: 1})
+            .call();
+        return { value, logs };
+    } catch(err: any) {
+        console.error("WETH TS SDK: " + err);
+        return err;
+    }
+}
+
+export async function transfer(
+    contractId: string,
+    walletPublicKey: string,
+    to: string,
+    amount: number,
+) {
+    try {
+        const wallet = new BaseWalletUnlocked(walletPublicKey);
+        const contract = WethAbi__factory.connect(contractId, wallet);
+        const _to: IdentityInput = { Address: { value: to } };
+        const { value, logs } = await contract.functions
+            .transfer(_to, amount)
+            .txParams({gasPrice: 1, variableOutputs: 1})
+            .call();
+        return { value, logs };
+    } catch(err: any) {
+        console.error("WETH TS SDK: " + err);
+        return err;
+    }
+}
+
+export async function transferFrom(
+    contractId: string,
+    walletPublicKey: string,
+    from: string,
+    to: string,
+    amount: number,
+) {
+    try {
+        const wallet = new BaseWalletUnlocked(walletPublicKey);
+        const contract = WethAbi__factory.connect(contractId, wallet);
+        const _from: IdentityInput = { Address: { value: from } };
+        const _to: IdentityInput = { Address: { value: to } };
+        const { value, logs } = await contract.functions
+            .transfer_from(_from, _to, amount)
+            .txParams({gasPrice: 1, variableOutputs: 1})
+            .call();
+        return { value, logs };
+    } catch(err: any) {
+        console.error("WETH TS SDK: " + err);
+        return err;
+    }
+}
+
+export async function increaseAllowance(
+    contractId: string,
+    walletPublicKey: string,
+    spender: string,
+    addedAmount: number,
+) {
+    try {
+        const wallet = new BaseWalletUnlocked(walletPublicKey);
+        const contract = WethAbi__factory.connect(contractId, wallet);
+        const _spender: IdentityInput = { ContractId: { value: spender } };
+        const { value, logs } = await contract.functions
+            .increase_allowance(_spender, addedAmount)
+            .txParams({gasPrice: 1, variableOutputs: 1})
+            .call();
+        return { value, logs };
+    } catch(err: any) {
+        console.error("WETH TS SDK: " + err);
+        return err;
+    }
+}
+
+export async function decreaseAllowance(
+    contractId: string,
+    walletPublicKey: string,
+    spender: string,
+    subtractedAmount: number,
+) {
+    try {
+        const wallet = new BaseWalletUnlocked(walletPublicKey);
+        const contract = WethAbi__factory.connect(contractId, wallet);
+        const _spender: IdentityInput = { ContractId: { value: spender } };
+        const { value, logs } = await contract.functions
+            .decrease_allowance(_spender, subtractedAmount)
+            .txParams({gasPrice: 1, variableOutputs: 1})
+            .call();
+        return { value, logs };
     } catch(err: any) {
         console.error("WETH TS SDK: " + err);
         return err;
