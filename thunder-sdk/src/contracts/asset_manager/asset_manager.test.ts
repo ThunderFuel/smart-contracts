e4080b8151b34411a625e1dd5579045836a4f29e
meliksahgurtemel <meliksahgurtemel01@gmail.com>
2023-01-30 13:42:24 +0300

    feat: unit tests for asset manager

--- thunder-sdk/src/contracts/asset_manager/asset_manager.test.ts
+++ thunder-sdk/src/contracts/asset_manager/asset_manager.test.ts
@@ -1,9 +1,9 @@
 import fs from 'fs';
 import { ContractFactory, CreateTransactionRequest, Provider, TransactionCoder, transactionRequestify, WalletUnlocked } from 'fuels';
 import path from 'path';
-import { AssetManagerAbi__factory } from "../../types/contracts/factories/AssetManagerAbi__factory"
+import { AssetManagerAbi__factory } from "../../types/asset_manager/factories/AssetManagerAbi__factory"
 
-import { initialize, setup } from './asset_manager';
+import { initialize } from './asset_manager';
 
 const PROVIDER = new Provider('http://127.0.0.1:4000/graphql');
 const USER: WalletUnlocked = new WalletUnlocked("0xde97d8624a438121b86a1956544bd72ed68cd69f2c99555b08b1e8c51ffd511c", PROVIDER);
@@ -18,10 +18,7 @@ describe('AssetManager', () => {
     const contract = await factory.deployContract();
     console.log("3")
 
-    //const contract_id = "0x0505baedf17ef227563f99e7fbbda3f428bd38d4e206e8bb008e60ade7a81154"
-
-    await setup(contract.id.toString(), PROVIDER.url, USER.address.toString());
-    const result = await initialize();
+    const result = await initialize(contract.id.toString(), PROVIDER.url, USER.privateKey);
     console.log(result);
   });
 });
