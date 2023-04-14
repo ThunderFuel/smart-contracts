b18376ea0a13b05949f0cbcb0a812e7ebe0ca42f
Meliksah Gurtemel <meliksahgurtemel01@gmail.com>
2023-04-14 13:23:04 +0200

    feat: deploy script

--- thunder-sdk/src/contracts/thunder_exchange/deploy.ts
+++ thunder-sdk/src/contracts/thunder_exchange/deploy.ts
@@ -1,4 +1,4 @@
-import fs from 'fs';
+import { readFileSync } from "fs";
 import { Contract, ContractFactory, Provider, WalletUnlocked, NativeAssetId } from 'fuels';
 import path from 'path';
 
@@ -29,205 +29,229 @@ let assetManager: Contract;
 let transferSelector: Contract;
 let transferManager: Contract;
 let erc721: Contract;
-let strategy: Contract;
+let strategyFixedPrice: Contract;
+let strategyAuction: Contract;
 let exchange: Contract;
 let contracts: Exchange.Contracts;
 
-const PROVIDER = new Provider('http://127.0.0.1:4000/graphql');
-const OWNER = new WalletUnlocked("0xde97d8624a438121b86a1956544bd72ed68cd69f2c99555b08b1e8c51ffd511c", PROVIDER);
-const TM1155 = "0x1230000000000000000000000000000000000000000000000000000000000abc";
+const localTestnet = new Provider('http://127.0.0.1:4000/graphql');
+const beta3Testnet = new Provider("https://beta-3.fuel.network/graphql");
 
+const main = async (provider: Provider) => {
+    const OWNER = new WalletUnlocked("0xde97d8624a438121b86a1956544bd72ed68cd69f2c99555b08b1e8c51ffd511c", provider);
+    const TM1155 = "0x1230000000000000000000000000000000000000000000000000000000000abc";
 
-async function deployAll() {
-    // Deploy Exchange
-    const exchangeBytecode = fs.readFileSync(path.join(__dirname, '../../../../contracts-v1/thunder_exchange/out/debug/thunder_exchange.bin'));
-    const exchangeFactory = new ContractFactory(exchangeBytecode, ThunderExchangeAbi__factory.abi, OWNER);
-    exchange = await exchangeFactory.deployContract();
+    const deployAll = async () => {
+        // Deploy Exchange
+        const exchangeBytecode = readFileSync(path.join(__dirname, '../../../../contracts-v1/thunder_exchange/out/debug/thunder_exchange.bin'));
+        const exchangeFactory = new ContractFactory(exchangeBytecode, ThunderExchangeAbi__factory.abi, OWNER);
+        exchange = await exchangeFactory.deployContract({gasPrice: 1});
 
-    // Initialize Exchange
-    const { transactionResult: exchangeResult } = await Exchange.initialize(
-        exchange.id.toString(),
-        PROVIDER.url,
-        OWNER.privateKey,
-    );
-    expect(exchangeResult?.status.type).toBe("success");
-    const { value } = await Exchange.owner(
-        exchange.id.toString(),
-        PROVIDER.url,
-    );
-    expect(value?.Address?.value).toBe(OWNER.address.toB256());
+        // Initialize Exchange
+        await Exchange.initialize(
+            exchange.id.toString(),
+            provider.url,
+            OWNER.privateKey,
+        );
 
-    // Deploy AssetManager
-    const assetManagerBytecode = fs.readFileSync(path.join(__dirname, '../../../../contracts-v1/asset_manager/out/debug/asset_manager.bin'));
-    const assetManagerFactory = new ContractFactory(assetManagerBytecode, AssetManagerAbi__factory.abi, OWNER);
-    assetManager = await assetManagerFactory.deployContract();
+        await Exchange.owner(
+            exchange.id.toString(),
+            provider.url,
+        );
 
-    // Initialize AssetManager
-    const { transactionResult } = await AssetManager.initialize(
-        assetManager.id.toString(),
-        PROVIDER.url,
-        OWNER.privateKey
-    );
-    const { transactionResult: result } = await AssetManager.addAsset(
-        assetManager.id.toString(),
-        PROVIDER.url,
-        OWNER.privateKey,
-        NativeAssetId
-    );
-    expect(transactionResult?.status.type).toBe("success");
-    expect(result?.status.type).toBe("success");
+        // Deploy AssetManager
+        const assetManagerBytecode = readFileSync(path.join(__dirname, '../../../../contracts-v1/asset_manager/out/debug/asset_manager.bin'));
+        const assetManagerFactory = new ContractFactory(assetManagerBytecode, AssetManagerAbi__factory.abi, OWNER);
+        assetManager = await assetManagerFactory.deployContract({gasPrice: 1});
 
-    // Deploy Pool
-    const poolBytecode = fs.readFileSync(path.join(__dirname, '../../../../contracts-v1/pool/out/debug/pool.bin'));
-    const poolFactory = new ContractFactory(poolBytecode, PoolAbi__factory.abi, OWNER);
-    pool = await poolFactory.deployContract();
+        // Initialize AssetManager
+        await AssetManager.initialize(
+            assetManager.id.toString(),
+            provider.url,
+            OWNER.privateKey
+        );
 
-    // Initialize Pool
-    const { transactionResult: poolResult } = await Pool.initialize(
-        pool.id.toString(),
-        PROVIDER.url,
-        OWNER.privateKey,
-        exchange.id.toB256(),
-        assetManager.id.toB256()
-    );
-    expect(poolResult.status.type).toBe("success");
+        await AssetManager.addAsset(
+            assetManager.id.toString(),
+            provider.url,
+            OWNER.privateKey,
+            NativeAssetId
+        );
 
-    // Deploy Strategy
-    const strategyBytecode = fs.readFileSync(path.join(__dirname, '../../../../contracts-v1/execution_strategies/strategy_fixed_price_sale/out/debug/strategy_fixed_price_sale.bin'));
-    const strategyFactory = new ContractFactory(strategyBytecode, StrategyFixedPriceSaleAbi__factory.abi, OWNER);
-    strategy = await strategyFactory.deployContract();
+        // Deploy Pool
+        const poolBytecode = readFileSync(path.join(__dirname, '../../../../contracts-v1/pool/out/debug/pool.bin'));
+        const poolFactory = new ContractFactory(poolBytecode, PoolAbi__factory.abi, OWNER);
+        pool = await poolFactory.deployContract({gasPrice: 1});
 
-    // Initialize Strategy
-    const { transactionResult: sResult } = await Strategy.initialize(
-        strategy.id.toString(),
-        PROVIDER.url,
-        OWNER.privateKey,
-        exchange.id.toB256(),
-    );
-    expect(sResult?.status.type).toBe("success");
+        // Initialize Pool
+        await Pool.initialize(
+            pool.id.toString(),
+            provider.url,
+            OWNER.privateKey,
+            exchange.id.toB256(),
+            assetManager.id.toB256()
+        );
 
-    const { transactionResult: protocolFeeRes } = await Strategy.setProtocolFee(
-        strategy.id.toString(),
-        PROVIDER.url,
-        OWNER.privateKey,
-        250
-    );
-    expect(protocolFeeRes?.status.type).toBe("success");
+        // Deploy Strategy Fixed Price Sale
+        const strategyBytecode = readFileSync(path.join(__dirname, '../../../../contracts-v1/execution_strategies/strategy_fixed_price_sale/out/debug/strategy_fixed_price_sale.bin'));
+        const strategyFactory = new ContractFactory(strategyBytecode, StrategyFixedPriceSaleAbi__factory.abi, OWNER);
+        strategyFixedPrice = await strategyFactory.deployContract({gasPrice: 1});
 
-    // Deploy Execution Manager
-    const executionManagerBytecode = fs.readFileSync(path.join(__dirname, '../../../../contracts-v1/execution_manager/out/debug/execution_manager.bin'));
-    const executionManagerFactory = new ContractFactory(executionManagerBytecode, ExecutionManagerAbi__factory.abi, OWNER);
-    executionManager = await executionManagerFactory.deployContract();
+        // Initialize Strategy Fixed Price Sale
+        await Strategy.initialize(
+            strategyFixedPrice.id.toString(),
+            provider.url,
+            OWNER.privateKey,
+            exchange.id.toB256(),
+        );
 
-    // Initialize Execution Manager
-    const { transactionResult: executionManagerResult } = await ExecutionManager.initialize(
-        executionManager.id.toString(),
-        PROVIDER.url,
-        OWNER.privateKey
-    );
-    const { transactionResult: addStrategy } = await ExecutionManager.addStrategy(
-        executionManager.id.toString(),
-        PROVIDER.url,
-        OWNER.privateKey,
-        strategy.id.toB256()
-    );
-    expect(executionManagerResult.status.type).toBe("success");
-    expect(addStrategy.status.type).toBe("success");
+        await Strategy.setProtocolFee(
+            strategyFixedPrice.id.toString(),
+            provider.url,
+            OWNER.privateKey,
+            250
+        );
 
-    // Deploy Transfer Manager
-    const transferManagerBytecode = fs.readFileSync(path.join(__dirname, '../../../../contracts-v1/transfer_managers/transfer_manager_721/out/debug/transfer_manager_721.bin'));
-    const transferManagerFactory = new ContractFactory(transferManagerBytecode, TransferManager721Abi__factory.abi, OWNER);
-    transferManager = await transferManagerFactory.deployContract();
+        // Deploy Strategy Auction
+        const strategyAuctionBytecode = readFileSync(path.join(__dirname, '../../../../contracts-v1/execution_strategies/strategy_auction/out/debug/strategy_auction.bin'));
+        const strategyAuctionFactory = new ContractFactory(strategyAuctionBytecode, StrategyFixedPriceSaleAbi__factory.abi, OWNER);
+        strategyAuction = await strategyAuctionFactory.deployContract({gasPrice: 1});
 
-    // Initialize Transfer Manager
-    const { transactionResult: tmInitialize } = await TransferManager.initialize(
-        transferManager.id.toString(),
-        PROVIDER.url,
-        OWNER.privateKey,
-        exchange.id.toB256()
-    );
-    expect(tmInitialize.status.type).toBe("success");
+        // Initialize Strategy Auction
+        await Strategy.initialize(
+            strategyAuction.id.toString(),
+            provider.url,
+            OWNER.privateKey,
+            exchange.id.toB256(),
+        );
 
-    // Deploy Transfer Selector
-    const transferSelectorBytecode = fs.readFileSync(path.join(__dirname, '../../../../contracts-v1/transfer_selector/out/debug/transfer_selector.bin'));
-    const transferSelectorFactory = new ContractFactory(transferSelectorBytecode, TransferSelectorAbi__factory.abi, OWNER);
-    transferSelector = await transferSelectorFactory.deployContract();
+        await Strategy.setProtocolFee(
+            strategyAuction.id.toString(),
+            provider.url,
+            OWNER.privateKey,
+            250
+        );
 
-    // Initialize Transfer Selector
-    const { transactionResult: tsResult } = await TransferSelector.initialize(
-        transferSelector.id.toString(),
-        PROVIDER.url,
-        OWNER.privateKey,
-        transferManager.id.toB256(),
-        TM1155,
-    );
-    expect(tsResult.status.type).toBe("success");
+        // Deploy Execution Manager
+        const executionManagerBytecode = readFileSync(path.join(__dirname, '../../../../contracts-v1/execution_manager/out/debug/execution_manager.bin'));
+        const executionManagerFactory = new ContractFactory(executionManagerBytecode, ExecutionManagerAbi__factory.abi, OWNER);
+        executionManager = await executionManagerFactory.deployContract({gasPrice: 1});
 
-    // Deploy Royalty Manager
-    const royaltyManagerBytecode = fs.readFileSync(path.join(__dirname, '../../../../contracts-v1/royalty_manager/out/debug/royalty_manager.bin'));
-    const royaltyManagerFactory = new ContractFactory(royaltyManagerBytecode, RoyaltyManagerAbi__factory.abi, OWNER);
-    royaltyManager = await royaltyManagerFactory.deployContract();
+        // Initialize Execution Manager
+        await ExecutionManager.initialize(
+            executionManager.id.toString(),
+            provider.url,
+            OWNER.privateKey
+        );
+        await ExecutionManager.addStrategy(
+            executionManager.id.toString(),
+            provider.url,
+            OWNER.privateKey,
+            strategyFixedPrice.id.toB256()
+        );
+        await ExecutionManager.addStrategy(
+            executionManager.id.toString(),
+            provider.url,
+            OWNER.privateKey,
+            strategyAuction.id.toB256()
+        );
 
-    // Initialize Royalty Manager
-    const { transactionResult: rmResult } = await RoyaltyManager.initialize(
-        royaltyManager.id.toString(),
-        PROVIDER.url,
-        OWNER.privateKey
-    );
-    expect(rmResult.status.type).toBe("success");
+        // Deploy Transfer Manager
+        const transferManagerBytecode = readFileSync(path.join(__dirname, '../../../../contracts-v1/transfer_managers/transfer_manager_721/out/debug/transfer_manager_721.bin'));
+        const transferManagerFactory = new ContractFactory(transferManagerBytecode, TransferManager721Abi__factory.abi, OWNER);
+        transferManager = await transferManagerFactory.deployContract({gasPrice: 1});
 
-    const { transactionResult: royaltyFeeRes } = await RoyaltyManager.setRoyaltyFeeLimit(
-        royaltyManager.id.toString(),
-        PROVIDER.url,
-        OWNER.privateKey,
-        1000
-    );
-    expect(royaltyFeeRes.status.type).toBe("success");
+        // Initialize Transfer Manager
+        await TransferManager.initialize(
+            transferManager.id.toString(),
+            provider.url,
+            OWNER.privateKey,
+            exchange.id.toB256()
+        );
 
-    // Deploy NFT
-    const nftBytecode = fs.readFileSync(path.join(__dirname, '../../../../contracts-v1/erc721/out/debug/NFT.bin'));
-    const nftFactory = new ContractFactory(nftBytecode, NFTAbi__factory.abi, OWNER);
-    erc721 = await nftFactory.deployContract();
+        // Deploy Transfer Selector
+        const transferSelectorBytecode = readFileSync(path.join(__dirname, '../../../../contracts-v1/transfer_selector/out/debug/transfer_selector.bin'));
+        const transferSelectorFactory = new ContractFactory(transferSelectorBytecode, TransferSelectorAbi__factory.abi, OWNER);
+        transferSelector = await transferSelectorFactory.deployContract({gasPrice: 1});
 
-    // Initialize NFT
-    const { transactionResult: nftResult } = await ERC721.initialize(
-        erc721.id.toString(),
-        PROVIDER.url,
-        OWNER.privateKey,
-        10000,
-        transferManager.id.toB256()
-    );
-    expect(nftResult.status.type).toBe("success");
+        // Initialize Transfer Selector
+        await TransferSelector.initialize(
+            transferSelector.id.toString(),
+            provider.url,
+            OWNER.privateKey,
+            transferManager.id.toB256(),
+            TM1155,
+        );
 
-    const { transactionResult: registerRes } = await RoyaltyManager.registerRoyaltyInfo(
-        royaltyManager.id.toString(),
-        PROVIDER.url,
-        OWNER.privateKey,
-        erc721.id.toB256(),
-        OWNER.address.toB256(),
-        true,
-        500
-    );
-    expect(registerRes.status.type).toBe("success");
+        // Deploy Royalty Manager
+        const royaltyManagerBytecode = readFileSync(path.join(__dirname, '../../../../contracts-v1/royalty_manager/out/debug/royalty_manager.bin'));
+        const royaltyManagerFactory = new ContractFactory(royaltyManagerBytecode, RoyaltyManagerAbi__factory.abi, OWNER);
+        royaltyManager = await royaltyManagerFactory.deployContract({gasPrice: 1});
 
-    const nftContract = NFTAbi__factory.connect(erc721.id, OWNER);
-    const { transactionResult: mintResult } = await nftContract.functions.mint(20, { Address: { value: OWNER.address.toB256() } })
-        .txParams({gasPrice: 1})
-        .call()
-    const { transactionResult: approvalResult } = await nftContract.functions.setApprovalForAll({ ContractId: { value: transferManager.id.toB256() } }, true)
-        .txParams({gasPrice: 1})
-        .call()
+        // Initialize Royalty Manager
+        await RoyaltyManager.initialize(
+            royaltyManager.id.toString(),
+            provider.url,
+            OWNER.privateKey
+        );
 
-    expect(mintResult.status.type).toBe("success");
-    expect(approvalResult.status.type).toBe("success");
+        await RoyaltyManager.setRoyaltyFeeLimit(
+            royaltyManager.id.toString(),
+            provider.url,
+            OWNER.privateKey,
+            1000
+        );
 
-    contracts = {
-        pool: pool.id.toB256(),
-        executionManager: executionManager.id.toB256(),
-        royaltyManager: royaltyManager.id.toB256(),
-        assetManager: assetManager.id.toB256(),
-        transferSelector: transferSelector.id.toB256(),
-        transferManager: transferManager.id.toB256(),
+        // Deploy NFT
+        const nftBytecode = readFileSync(path.join(__dirname, '../../../../contracts-v1/erc721/out/debug/NFT.bin'));
+        const nftFactory = new ContractFactory(nftBytecode, NFTAbi__factory.abi, OWNER);
+        erc721 = await nftFactory.deployContract({gasPrice: 1});
+
+        // Initialize NFT
+        await ERC721.initialize(
+            erc721.id.toString(),
+            provider.url,
+            OWNER.privateKey,
+            10000,
+            transferManager.id.toB256()
+        );
+
+        await RoyaltyManager.registerRoyaltyInfo(
+            royaltyManager.id.toString(),
+            provider.url,
+            OWNER.privateKey,
+            erc721.id.toB256(),
+            OWNER.address.toB256(),
+            true,
+            500
+        );
+
+        const nftContract = NFTAbi__factory.connect(erc721.id, OWNER);
+        await nftContract.functions.mint(20, { Address: { value: OWNER.address.toB256() } })
+            .txParams({gasPrice: 1})
+            .call()
+        await nftContract.functions.setApprovalForAll({ ContractId: { value: transferManager.id.toB256() } }, true)
+            .txParams({gasPrice: 1})
+            .call()
+
+        contracts = {
+            pool: pool.id.toB256(),
+            executionManager: executionManager.id.toB256(),
+            royaltyManager: royaltyManager.id.toB256(),
+            assetManager: assetManager.id.toB256(),
+            transferSelector: transferSelector.id.toB256(),
+            transferManager: transferManager.id.toB256(),
+        }
+        Exchange.setContracts(contracts, provider);
+        console.log([contracts, exchange.id.toB256(), erc721.id.toB256(), strategyFixedPrice.id.toB256(), strategyAuction.id.toB256()])
+        return contracts
     }
-    console.log(contracts)
+
+    await deployAll()
 }
+
+main(localTestnet)
+    .then((res) => {
+        return res;
+    })
+    .catch((err) => console.log(err));
